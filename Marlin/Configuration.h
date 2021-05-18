3c3010a8fce3a9274e8de6ba161ba4b58be1aad1
KvassHardbass <75281782+KvassHardbass@users.noreply.github.com>
2021-05-18 02:29:17 +0200

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -65,7 +65,7 @@
   #define WITH_TITAN 1
   #define X_BED_SIZE 330
   #define Y_BED_SIZE 330
-  #define Z_MAX_POS 400
+  #define Z_MAX_POS 350
   #define CUSTOM_MACHINE_NAME "Tronxy X5SA V6 330 Titan TMC"
   #define COREXY
 
@@ -1529,7 +1529,7 @@
 #define X_MIN_POS 0
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE
+#define X_MAX_POS X_BED_SIZE + 15
 #define Y_MAX_POS Y_BED_SIZE
 
 /**
